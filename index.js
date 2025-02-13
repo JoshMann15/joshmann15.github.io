6a1f3fcf4bef27891fbc58bb80ca17c41b85f027
JoshMann15 <joshmann15dev@gmail.com>
2025-02-13 20:44:15 +1100

    V3.1 🔥

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const headings = document.querySelectorAll('.content h1, .content h2');
+
+    headings.forEach(function (heading) {
+        heading.addEventListener('animationend', function (e) {
+            if (e.animationName === 'fadeIn') {
+                heading.style.pointerEvents = 'auto';
+            }
+        });
+    });
+});
